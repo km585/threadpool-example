1823ca6a1e59959b77aeb0e7fe2235657b24fde9
km585 <57662022+km585@users.noreply.github.com>
2019-11-12 11:27:04 +0000

    Create Job.java

---
+++ Job.java
@@ -0,0 +1,18 @@
+public class Job implements Runnable{
+    private int jobNumber;
+    
+    job (int jobNumber){
+        this.jobNumber=jobNumber;
+    }
+    
+    public void run(){
+        try {
+            Thread.sleep((int)1000) 
+        }catch (InterruptionException e){
+        
+        }
+        
+        System.out.println("Job: " + jobNumber + " is ending in thread : "+ + Thread.currentThread().getName());
+    }   
+        
+}
